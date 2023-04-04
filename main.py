a44d0b4ffcdc58fa5864c35f5675ae9fc813c653
Enqrypted <56021157+Enqrypted@users.noreply.github.com>
2023-04-04 14:13:04 +0200

    a

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def serve_plot():
     return send_file(buffer, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    app.run(debug=True, port=8083)
 
 start_time = time.time()
 for _ in range(num_days):
